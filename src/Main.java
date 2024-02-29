f82a73edd8fbeb8bcab5f69e131d7e79ed898255
MariMax <maxim.prohachev@yandex.ru>
2024-02-29 21:25:28 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 25312;
+        int bonus = 20;
+        int miles = ticket / bonus;
+        System.out.println(miles);
+    }
+}
